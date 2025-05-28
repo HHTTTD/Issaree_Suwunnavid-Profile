38ffcad7763c9587f75270d462a26674ec5c6404
Sitthisak <sitthisak.srs@spumail.net>
2025-05-28 12:22:45 +0700

    new file:   assets/css/fade.css 	new file:   assets/js/script.js 	modified:   index.html

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// fade.js
+document.addEventListener("DOMContentLoaded", function () {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // ป้องกันการดูซ้ำ
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  faders.forEach(fader => {
+    observer.observe(fader);
+  });
+});
